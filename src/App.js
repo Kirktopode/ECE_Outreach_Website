b81688e14c5df1ac94483b1828d054ebc250ab5b
Shizcow <pohl.devin@gmail.com>
2020-09-25 11:30:38 -0600

    Got the card basic animations down. Hover + icons + breathing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+class Card extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    hovering: false
+	};
+	// Set up 
+	let arr = React.Children.toArray(this.props.children);
+	this.elements = [];
+	for(let i=0; i<arr.length; ++i) {
+	    this.elements[i] = React.cloneElement(arr[i], {
+		style: {"--angle": (i/arr.length*360-90) + "deg"}}, null);
+	}
+    }
+    
+    render() {
+	let elements = [];
+	for(let i=0; i<this.elements.length; ++i) {
+	    elements[i] = React.cloneElement(this.elements[i], {
+		className: ("breathing hover " + (this.state.hovering ? "hovering" : ""))
+	    }, null);
+	}
+	return (<div className="card"
+		     onMouseLeave={() => this.setState({hovering: false})}>
+		    <img alt="Something interesting"
+			 className={`card_center ${this.state.hovering ? "" : "breathing"}`}
+			 onMouseEnter={() => this.setState({hovering: true})}
+			 src={require('./center.jpg')}
+		    />
+		    {elements}
+		</div>);
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+	<div className="App">
+	    <header className="App-header">
+		<Card>
+		    <img alt="Hover"
+			 src={require('./hover.png')}
+		    />
+		    <img alt="Hover"
+			 src={require('./hover.png')}
+		    />
+		    <img alt="Hover"
+			 src={require('./hover.png')}
+		    />
+		</Card>
+	    </header>
+	</div>
+    );
 }
 
 export default App;
