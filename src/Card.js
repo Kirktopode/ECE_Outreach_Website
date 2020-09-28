4c2659e7c46f404e2dc3bf7c18a3d9da0d671e6f
Shizcow <pohl.devin@gmail.com>
2020-09-28 15:17:28 -0600

    dropdown is now picture-aware of what's clicked on

--- src/Card.js
+++ src/Card.js
@@ -5,51 +5,67 @@ class Card extends React.Component {
 	super(props);
 	this.state = {
 	    hovering: false,
-	    open: false
+	    open: null,
 	};
 	// Set up
 	const degmax = 150;
-	let arr = React.Children.toArray(this.props.children);
+	this.intake_children = React.Children.toArray(this.props.children);
 	
-	this.center = React.cloneElement(arr.shift(), {
+	this.center = React.cloneElement(this.intake_children.shift(), {
 	    onMouseEnter:() => {
 		if(!this.state.hovering) {
-		    this.setState({open: false});
+		    this.setState({open: null});
 		    this.setState({hovering: true});
 		}
 	    }
 	}, null);
 	
-	this.elements = [];
-	for(let i=0; i<arr.length; ++i) {
-	    let angle = i/arr.length*360;
+	this.hover_elements = [];
+	for(let i=0; i<this.intake_children.length; ++i) {
+	    let angle = i/this.intake_children.length*360;
 	    let angle_open;
-	    if(arr.length%2===1) {
-		if(i<arr.length/2) {
-		    angle_open = (i+1+Math.floor(arr.length/2))/(arr.length+1)*2*degmax-degmax;
+	    if(this.intake_children.length%2===1) {
+		if(i<this.intake_children.length/2) {
+		    angle_open = (i+1+Math.floor(this.intake_children.length/2))/(this.intake_children.length+1)*2*degmax-degmax;
 		} else {
-		    angle_open = (i-Math.floor(arr.length/2))/(arr.length+1)*2*degmax-degmax+360;
+		    angle_open = (i-Math.floor(this.intake_children.length/2))/(this.intake_children.length+1)*2*degmax-degmax+360;
 		}
 	    } else {
-		if(i<=arr.length/2) {
-		    angle_open = (i+arr.length/2)/(arr.length+1)*2*degmax-degmax;
+		if(i<=this.intake_children.length/2) {
+		    angle_open = (i+this.intake_children.length/2)/(this.intake_children.length+1)*2*degmax-degmax;
 		} else {
-		    angle_open = (i+arr.length/2-arr.length)/(arr.length+1)*2*degmax-degmax+360;
+		    angle_open = (i+this.intake_children.length/2-this.intake_children.length)/(this.intake_children.length+1)*2*degmax-degmax+360;
 		}
 	    }
-	    this.elements[i] = React.cloneElement(arr[i], {
+	    this.hover_elements[i] = React.cloneElement(this.intake_children[i], {
 		style: {"--angle": (angle-90) + "deg",
 			"--angle-open": (angle_open-90) + "deg"},
-		onClick:() => this.setState({open: true})}, null);
+		onClick:() => this.setState({open: i})}, null);
 	}
     }
     
     render() {
-	let elements = [];
-	for(let i=0; i<this.elements.length; ++i) {
-	    elements[i] = React.cloneElement(this.elements[i], {
+	let dropdown_elements = [];
+	for(let i=0; i<this.intake_children.length; ++i) {
+	    dropdown_elements[i] = (
+		<div className={"card_dropdown" + (this.state.open === i && this.state.hovering ? "" : " folded")}>
+		    {this.intake_children[i]}
+		    <div className="card_title_wrapper">
+			<div className={"card_title_mover" + (this.state.open === i && this.state.hovering ? "" : " folded")}>
+			    <p>
+				History of the Project
+			    </p>
+			</div>
+		    </div>
+		</div>
+	    );
+	}
+	
+	let hover_elements = [];
+	for(let i=0; i<this.hover_elements.length; ++i) {
+	    hover_elements[i] = React.cloneElement(this.hover_elements[i], {
 		className: ("hover " + (this.state.hovering ? "breathing hovering " : "")
-			    + (this.state.open ? "open" : ""))
+			    + (this.state.open !== null ? "open" : ""))
 	    }, null);
 	}
 	let center = React.cloneElement(this.center, {
@@ -59,22 +75,11 @@ class Card extends React.Component {
 		     onMouseLeave={() => {this.setState({hovering: false});
 					  setTimeout(() => {
 					      if(!this.state.hovering)
-						  this.setState({open: false});
+						  this.setState({open: null});
 					  }, 500)}}>
 		    {center}
-		    {elements}
-		    <div className={"card_dropdown" + (this.state.open && this.state.hovering ? "" : " folded")}>
-			<img alt="Lessons"
-			     src={require('./images/classroom.png')}
-			/>
-			<div className="card_title_wrapper">
-			    <div className={"card_title_mover" + (this.state.open && this.state.hovering ? "" : " folded")}>
-				<p>
-				    History of the Project
-				</p>
-			    </div>
-			</div>
-		    </div>
+		    {hover_elements}
+		    {dropdown_elements}
 		</div>);
     }
 }
