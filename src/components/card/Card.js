9dcadf244075fc847964cb589a1fbd6a0b64bd7b
Jameson <masksofpurple@gmail.com>
2020-10-23 17:38:59 -0600

    putting this in again because i did a stupid

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -37,108 +37,118 @@ function syncGet(url) {
 class Card extends React.Component {
     static degmax = 150; // how far the hover elements move out of the way
     constructor(props) {
-	super(props);
-	this.state = {
-	    hovering: false,
-	    open: null,
-	};
-	// first check if we need to load JSON or if inline is okay
-	let children;
-	if(this.props.src) {
-	    let config = TOML.parse(syncGet(this.props.src + "/config.toml"));
-	    
-	    children = [
-		(<Icon alt={config.center.alt}
-		       src={`${this.props.src+'/'+config.center.src}`}
-		 />)];
-	    config.dropdown.forEach(dropdown => {
-		let content = syncGet(this.props.src+'/'+dropdown.content);
-		children.push(
-		    (<Dropdown>
-			 <Icon alt=""
-			       src={require(`${lookupIconName(dropdown.icon)}`)}
-			 />
-			 <Label>
-			     {dropdown.label}
-			 </Label>
-			 <Content dangerouslySetInnerHTML={ { __html: content } }/>
-		     </Dropdown>));
-	    });
-	} else {
-	    children = this.props.children;
-	}
-	
-	this.dropdown_elements = React.Children.toArray(children)
-	    .filter((child) => child.type.name === "Dropdown");
+		super(props);
+		this.state = {
+			hovering: false,
+			open: null,
+		};
+		// first check if we need to load JSON or if inline is okay
+		let children;
+		if(this.props.src) {
+			let config = TOML.parse(syncGet(this.props.src + "/config.toml"));
 
-	let center = React.Children.toArray(children)
-	    .filter((child) => child.type.name === "Icon")[0];
-	this.center = React.cloneElement(center, {
-	    onMouseEnter:() => {
-		if(!this.state.hovering) {
-		    this.setState({open: null});
-		    this.setState({hovering: true});
+			children = [
+			(<Icon alt={config.center.alt}
+				   src={`${this.props.src+'/'+config.center.src}`}
+			 />)];
+			config.dropdown.forEach(dropdown => {
+			let content = syncGet(this.props.src+'/'+dropdown.content);
+			children.push(
+				(<Dropdown>
+				 <Icon alt=""
+					   src={require(`${lookupIconName(dropdown.icon)}`)}
+				 />
+				 <Label>
+					 {dropdown.label}
+				 </Label>
+				 <Content dangerouslySetInnerHTML={ { __html: content } }/>
+				 </Dropdown>));
+			});
+		} else {
+			children = this.props.children;
 		}
-	    }
-	}, React.Children.toArray(center.props.children));
 
-	this.hover_elements = [];
-	for(let i=0; i<this.dropdown_elements.length; ++i) {
-	    let angle = i/this.dropdown_elements.length*360;
-	    let angle_open;
-	    if(this.dropdown_elements.length%2===1) {
-		if(i<this.dropdown_elements.length/2) {
-		    angle_open = (i+1+Math.floor(this.dropdown_elements.length/2))/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax;
-		} else {
-		    angle_open = (i-Math.floor(this.dropdown_elements.length/2))/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax+360;
+		this.dropdown_elements = React.Children.toArray(children)
+			.filter((child) => {
+				if (child.type.name.length > 2)
+					return child.type.name === "Dropdown"
+				else
+					return child.props.children
+			});
+
+		let center = React.Children.toArray(children)
+			.filter((child) => {
+				if (child.type.name.length > 2)
+					return child.type.name === "Icon"
+				else
+					return !child.props.children
+			})[0];
+		this.center = React.cloneElement(center, {
+			onMouseEnter:() => {
+			if(!this.state.hovering) {
+				this.setState({open: null});
+				this.setState({hovering: true});
+			}
+			}
+		}, React.Children.toArray(center.props.children));
+
+		this.hover_elements = [];
+		for(let i=0; i<this.dropdown_elements.length; ++i) {
+			let angle = i/this.dropdown_elements.length*360;
+			let angle_open;
+			if(this.dropdown_elements.length%2===1) {
+			if(i<this.dropdown_elements.length/2) {
+				angle_open = (i+1+Math.floor(this.dropdown_elements.length/2))/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax;
+			} else {
+				angle_open = (i-Math.floor(this.dropdown_elements.length/2))/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax+360;
+			}
+			} else {
+			if(i<=this.dropdown_elements.length/2) {
+				angle_open = (i+this.dropdown_elements.length/2)/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax;
+			} else {
+				angle_open = (i+this.dropdown_elements.length/2-this.dropdown_elements.length)/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax+360;
+			}
+			}
+			this.hover_elements[i] = React.cloneElement(this.dropdown_elements[i], {
+			style: {"--angle": (angle-90) + "deg",
+				"--angle-open": (angle_open-90) + "deg"},
+			onClick:() => this.setState({open: i}),
+			mode: "hover"}, this.dropdown_elements[i].props.children);
 		}
-	    } else {
-		if(i<=this.dropdown_elements.length/2) {
-		    angle_open = (i+this.dropdown_elements.length/2)/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax;
-		} else {
-		    angle_open = (i+this.dropdown_elements.length/2-this.dropdown_elements.length)/(this.dropdown_elements.length+1)*2*Card.degmax-Card.degmax+360;
-		}
-	    }
-	    this.hover_elements[i] = React.cloneElement(this.dropdown_elements[i], {
-		style: {"--angle": (angle-90) + "deg",
-			"--angle-open": (angle_open-90) + "deg"},
-		onClick:() => this.setState({open: i}),
-		mode: "hover"}, this.dropdown_elements[i].props.children);
-	}
     }
     
     render() {
-	if(!this.dropdown_elements)
-	    return null;
-	let dropdown_elements = [];
-	for(let i=0; i<this.dropdown_elements.length; ++i) {
-	    dropdown_elements[i] = React.cloneElement(this.dropdown_elements[i], {
-		mode: "expand",
-		folded: (this.state.open !== i || !this.state.hovering).toString(),
-	    }, this.dropdown_elements[i].props.children);
-	}
-	
-	let hover_elements = [];
-	for(let i=0; i<this.hover_elements.length; ++i) {
-	    hover_elements[i] = React.cloneElement(this.hover_elements[i], {
-		className: ("hover " + (this.state.hovering ? " hovering " : "")
-			    + (this.state.open !== i ? " breathing" : "")
-			    + (this.state.open !== null ? " open" : "")),
-	    }, this.hover_elements[i].props.children);
-	}
-	let center = React.cloneElement(this.center, {
-	    className: ("card_center " + (this.state.hovering ? "" : "breathing"))
-	}, this.center.props.children);
-	return (<div className="card"
-		     onMouseLeave={() => {this.setState({hovering: false});
-					  setTimeout(() => {
-					      if(!this.state.hovering)
-						  this.setState({open: null});
-					  }, 500)}}>
-		    {center}
-		    {hover_elements}
-		    {dropdown_elements}
-		</div>);
+		if(!this.dropdown_elements)
+			return null;
+		let dropdown_elements = [];
+		for(let i=0; i<this.dropdown_elements.length; ++i) {
+			dropdown_elements[i] = React.cloneElement(this.dropdown_elements[i], {
+			mode: "expand",
+			folded: (this.state.open !== i || !this.state.hovering).toString(),
+			}, this.dropdown_elements[i].props.children);
+		}
+
+		let hover_elements = [];
+		for(let i=0; i<this.hover_elements.length; ++i) {
+			hover_elements[i] = React.cloneElement(this.hover_elements[i], {
+			className: ("hover " + (this.state.hovering ? " hovering " : "")
+					+ (this.state.open !== i ? " breathing" : "")
+					+ (this.state.open !== null ? " open" : "")),
+			}, this.hover_elements[i].props.children);
+		}
+		let center = React.cloneElement(this.center, {
+			className: ("card_center " + (this.state.hovering ? "" : "breathing"))
+		}, this.center.props.children);
+		return (<div className="card"
+				 onMouseLeave={() => {this.setState({hovering: false});
+						  setTimeout(() => {
+							  if(!this.state.hovering)
+							  this.setState({open: null});
+						  }, 500)}}>
+				{center}
+				{hover_elements}
+				{dropdown_elements}
+			</div>);
     }
 }
 
