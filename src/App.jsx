5fbd57f10cc1604b3fc475f0a8cb6cd9d498e553
Jameson <masksofpurple@gmail.com>
2020-10-23 15:49:05 -0600

    integrate cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Navigation from './components/navigation';
 import Header from './components/header';
 import Features from './components/features';
 import About from './components/about';
-import Services from './components/services';
+// import Services from './components/services';
 import Gallery from './components/gallery';
-import Testimonials from './components/testimonials';
-import Team from './components/Team';
+// import Testimonials from './components/testimonials';
+// import Team from './components/Team';
 import Contact from './components/contact';
 import JsonData from './data/data.json';
+import Card from "./Card";
 
 export class App extends Component {
   state = {
@@ -29,10 +30,10 @@ export class App extends Component {
         <Header data={this.state.landingPageData.Header} />
         <Features data={this.state.landingPageData.Features} />
         <About data={this.state.landingPageData.About} />
-        <Services data={this.state.landingPageData.Services} />
+        {/*<Services data={this.state.landingPageData.Services} />*/}
         <Gallery />
-        <Testimonials data={this.state.landingPageData.Testimonials} />
-        <Team data={this.state.landingPageData.Team} />
+        {/*<Testimonials data={this.state.landingPageData.Testimonials} />*/}
+        {/*<Team data={this.state.landingPageData.Team} />*/}
         <Contact data={this.state.landingPageData.Contact} />
       </div>
     )
