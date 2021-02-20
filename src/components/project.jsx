9a503e19d8630c7a4cb410ea0605190b023761a5
Shizcow <pohl.devin@gmail.com>
2021-02-19 17:22:38 -0700

    Added 'in progress' description to unfinigned 'Read Further' pages

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -10,21 +10,41 @@ export class Project extends Component {
         this.config = getTOML(this.projectID);
     }
     render() {
-        let content = getCardItem(this.projectID, this.config.center.main_page);
-        return (
-            <div id="portfolio" className="text-center">
-              <div className="container">
-                <div className="section-title">
-                  <h2>{this.config.center.title}</h2>
-                  <p>
-                    {this.config.center.description}
-                  </p>
+        if(this.config.center.main_page==="") {
+            // special case: unfinished
+            return (
+                <div id="portfolio" className="text-center">
+                  <div className="container">
+                    <div className="section-title">
+                      <h2>{this.config.center.title}</h2>
+                      <p>
+                        {this.config.center.description}
+                      </p>
+                    </div>
+                    <div>
+                      <h1>In Progress</h1>
+                      We are still working on adding exciting informational content to this page. Please check again soon!
+		    </div>
+                  </div>
                 </div>
-                <div dangerouslySetInnerHTML={{__html: content}}>
-		</div>
-              </div>
-            </div>
-        );
+            );
+        } else {
+            let content = getCardItem(this.projectID, this.config.center.main_page);
+            return (
+                <div id="portfolio" className="text-center">
+                  <div className="container">
+                    <div className="section-title">
+                      <h2>{this.config.center.title}</h2>
+                      <p>
+                        {this.config.center.description}
+                      </p>
+                    </div>
+                    <div dangerouslySetInnerHTML={{__html: content}}>
+		    </div>
+                  </div>
+                </div>
+            );
+        }
     }
 }
 
