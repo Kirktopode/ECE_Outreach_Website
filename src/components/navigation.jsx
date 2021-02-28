0bf2d917c04e1a5f94bf0ed722e8454d2976ef6f
Shizcow <pohl.devin@gmail.com>
2021-02-28 11:51:54 -0700

    Changed header to fit CSU guidelines

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -4,59 +4,98 @@ import {
 } from "react-router-dom";
 
 export class Navigation extends Component {
-  render() {
-    return (
-      <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
-            >
-              {" "}
-              <span className="sr-only">Toggle navigation</span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-              <span className="icon-bar"></span>{" "}
-            </button>
-            <Link className="navbar-brand page-scroll" to="about">
-              ECE Outreach
-            </Link>{" "}
-          </div>
+    render() {
+        if(true) {
+            return (
+                <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+                  <div className="container">
+                    <div className="navbar-header">
+                      <a href="https://www.colostate.edu"><img src={process.env.PUBLIC_URL + "/img/CSU_logo.png"} alt="" /></a>
+                      |
+                      <Link className="navbar-title" to="about">
+                        ECE Outreach
+                      </Link>{" "}
+                    </div>
 
-          <div
-            className="collapse navbar-collapse"
-            id="bs-example-navbar-collapse-1"
-          >
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to={process.env.PUBLIC_URL + "/about"} className="page-scroll">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to={process.env.PUBLIC_URL + "/gallery"} className="page-scroll">
-                  Projects
-                </Link>
-              </li>
-              <li>
-                <Link to={process.env.PUBLIC_URL + "/team"} className="page-scroll">
-                  Team
-                </Link>
-              </li>
-              <li>
-                <Link to={process.env.PUBLIC_URL + "/contact"} className="page-scroll">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+                    <ul className="nav navbar-nav navbar-right">
+                      <li>
+                        <Link to={process.env.PUBLIC_URL + "/about"} className="page-scroll">
+                          About
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to={process.env.PUBLIC_URL + "/gallery"} className="page-scroll">
+                          Projects
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to={process.env.PUBLIC_URL + "/team"} className="page-scroll">
+                          Team
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to={process.env.PUBLIC_URL + "/contact"} className="page-scroll">
+                          Contact
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+            );
+        } else {
+            return (
+                <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+                  <div className="container">
+                    <div className="navbar-header">
+                      <button
+                        type="button"
+                        className="navbar-toggle collapsed"
+                        data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1"
+                      >
+                        {" "}
+                        <span className="sr-only">Toggle navigation</span>{" "}
+                        <span className="icon-bar"></span>{" "}
+                        <span className="icon-bar"></span>{" "}
+                        <span className="icon-bar"></span>{" "}
+                      </button>
+                      <Link className="navbar-brand page-scroll" to="about">
+                        ECE Outreach
+                      </Link>{" "}
+                    </div>
+
+                    <div
+                      className="collapse navbar-collapse"
+                      id="bs-example-navbar-collapse-1"
+                    >
+                      <ul className="nav navbar-nav navbar-right">
+                        <li>
+                          <Link to={process.env.PUBLIC_URL + "/about"} className="page-scroll">
+                            About
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to={process.env.PUBLIC_URL + "/gallery"} className="page-scroll">
+                            Projects
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to={process.env.PUBLIC_URL + "/team"} className="page-scroll">
+                            Team
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to={process.env.PUBLIC_URL + "/contact"} className="page-scroll">
+                            Contact
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+            );
+        }
+    }
 }
 
 export default Navigation;
