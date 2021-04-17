a47716f08a7a545f689eace1fb6ee1597a0f4d36
kirktopode <masksofpurple@gmail.com>
2021-04-16 18:58:35 -0600

    add alumni, adjust to 5 per column

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,53 +1,114 @@
 import React, { Component } from "react";
+import { SocialIcon } from 'react-social-icons';
+// import Tabs from '@material-ui/core/Tabs';
+// import Tab from '@material-ui/core/Tab';
 
 export class Team extends Component {
   render() {
+    console.log(this.props)
+    const { teamData, alumData } = this.props
+    console.log({teamData, alumData})
     return (
-      <div id="team" className="text-center">
-        <div className="container">
-          <div className="col-md-8 col-md-offset-2 section-title">
-            <h2>Meet the Team</h2>
-            <p>
-              Meet our wonderful team of electrical and computer engineering students!
-            </p>
-          </div>
-          <div id="row">
-            {this.props.data
-              ? this.props.data.map((d, i) => (
-                  <div  key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+      <div>
+        <div id="team" className="text-center">
+          <div className="container">
+            <div className="col-md-8 col-md-offset-2 section-title">
+              <h2>Meet the Team</h2>
+              <p>
+                Meet our wonderful team of electrical and computer engineering students!
+              </p>
+            </div>
+            <div id="row">
+              {this.props.teamData
+                ? this.props.teamData.map((d, i) => (
                     <div
-                      className="thumbnail" 
-                      style={{
-                        height: '520px',
-                        textAlign: 'center'
-                      }}>
-                      {" "}
-                      <div 
-                      style={{
-                        height: '320px',
-                        position: 'relative',
-                        width: '220px',
-                        overflow: 'hidden',
-                        left: 15
-                      }}>
-                        <img src={d.img} alt={d.name} className="team-img" 
-                          style={{
-                            position: 'absolute',
-                            bottom: 0,
-                            left: -10
-                          }}/>
-                      </div>
-                      <div className="caption">
-                        <h4>{d.name}</h4>
-                        <p>{d.job}</p>
-                        <p>{d.description || "..."}</p>
+                      key={`${d.name}-${i}`}
+                      className={i % 5 == 0? "col-md-2 col-md-offset-1 col-sm-6 team" : "col-md-2 col-sm-6 team"}
+                    >
+                      <div
+                        className="thumbnail" 
+                        style={{
+                          height: '560px',
+                          textAlign: 'center'
+                        }}>
+                        {" "}
+                        <div 
+                        style={{
+                          height: '320px',
+                          position: 'relative',
+                          width: '220px',
+                          overflow: 'hidden',
+                          left: -10
+                        }}>
+                          <img src={d.img} alt={d.name} className="team-img" 
+                            style={{
+                              position: 'absolute',
+                              bottom: 0,
+                              left: -10
+                            }}/>
+                        </div>
+                        <div className="caption">
+                          <h4>{d.name}</h4>
+                          <p>{d.job}</p>
+                          <p>{d.description || "..."}</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))
-              : "loading"}
+                  ))
+                : "loading"}
+            </div>
+          </div>
+          <div className="container">
+            <div className="col-md-8 col-md-offset-2 section-title">
+              <h2>Alumni</h2>
+              <p>
+                These former members of ECE Outreach are now building exciting careers in electrical and computer engineering!
+              </p>
+            </div>
+            <div id="row">
+              {this.props.alumData
+                ? this.props.alumData.map((d, i) => (
+                    
+                    <div
+                      key={`${d.name}-${i}`}
+                      className={i % 5 == 0? "col-md-2 col-md-offset-1 col-sm-6 team" : "col-md-2 col-sm-6 team"}
+                    >
+                      <div
+                        className="thumbnail" 
+                        style={{
+                          height: '520px',
+                          textAlign: 'center'
+                        }}>
+                        {" "}
+                        <div 
+                        style={{
+                          height: '320px',
+                          position: 'relative',
+                          width: '220px',
+                          overflow: 'hidden',
+                          left: -10
+                        }}>
+                          <img src={d.img} alt={d.name} className="team-img" 
+                            style={{
+                              position: 'absolute',
+                              bottom: 0,
+                              left: -10
+                            }}/>
+                        </div>
+                        <div className="caption">
+                          <h4>{d.name}</h4>
+                          <p>{d.graduation}</p>
+                          <p>{d.quote}</p>
+                          <p><SocialIcon url={d.linkedin} /></p>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : "loading"}
+            </div>
           </div>
         </div>
+
       </div>
     );
   }
